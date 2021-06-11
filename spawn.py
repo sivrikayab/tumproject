4a754d499f867669770b06b9c1bc8f707a26d73c
sivrikayab <sivrikayab@itu.edu.tr>
2021-06-11 19:47:44 +0200

    CRUD logic of rooms are updated

--- spawn.py
+++ spawn.py
@@ -60,7 +60,88 @@ def move_robot(pose):
     time.sleep(1)
 
     icub.change_pose(x=pose[0], y=pose[1], z=pose[2], ox=pose[3], oy=pose[4], oz=pose[5])
+
+def move_object(obj_name, pose):
+
+    obj = Adjuster(obj_name)
     
+    time.sleep(1)
+
+    obj.change_pose(x=pose[0], y=pose[1], z=pose[2], ox=pose[3], oy=pose[4], oz=pose[5])
+
+    room_objects = shelve.open("room_objects")
+    temp = room_objects["furniture"]
+    temp.append(obj_name)
+    room_objects["furniture"] = temp
+    room_objects.close()
+
+def _spawn_all_objects():
+
+    print("Spawning all objects...")
+
+    room_objects = shelve.open("room_objects")
+    room_objects["furniture"] = []
+    room_objects.close()
+
+    blank_position = (20,20,0)
+    blank_orientation = (0,0,0)
+
+    rooms = ["kitchen", "living_room", "bed_room", "office"]
+
+    for room in rooms:
+        print("Spawning " + room + " objects...")
+        path_to_room = path_to_models + room + '/'
+        folder_list = [x for x in os.listdir(path_to_room) if os.path.isdir(path_to_room + x)]
+        for folder in folder_list:
+            print("In " + str(folder) + "...")
+            is_single_object = str.isdigit(folder[-1])
+            
+            if is_single_object:
+                absolute_path = path_to_room + folder + '/'
+                _spawn_single_object(absolute_path, blank_position, blank_orientation, object_name=folder)
+            else:
+                path_to_object = path_to_room + folder + '/'
+                object_list = os.listdir(path_to_object)
+                for obj in object_list:
+                    absolute_path = path_to_object + obj + '/'
+                    _spawn_single_object(absolute_path, blank_position, blank_orientation, object_name=obj)
+                    
+
+    print("All objects are spawned successfully.")
+
+    room_objects = shelve.open("room_objects")
+    room_objects["furniture"] = []
+    room_objects.close()
+
+def _build_whole_room(room_name, layout_no, layout_file="layout.yaml"):
+
+    room_objects = shelve.open("room_objects")
+    room_objects["furniture"] = []
+    room_objects.close()
+
+    path_to_room = path_to_models + room_name + '/'
+    with open(path_to_room + layout_file) as f:
+        yaml_file = yaml.load(f)
+
+    seen_folder = {}
+    layout = "Layout" + str(layout_no)
+    for entity in yaml_file[layout]:
+        folder = entity["folder"]
+        if(folder == "icub"):
+            positions = entity["positions"]
+            position = random.choice(positions.values())
+            move_robot((position["x"], position["y"], position["z"], position["ox"], position["oy"], position["oz"]))
+            continue
+        positions = entity["position"]
+        absolute_path = path_to_room + folder + '/'
+        if "has_subfolder" in entity and entity["has_subfolder"]: # we go through sub models
+            if folder not in seen_folder: # if the sub-dir seen before
+                seen_folder[folder] = random_path_yielder(absolute_path)
+            folder = next(seen_folder[folder])
+            absolute_path += folder + '/'
+        pose = (positions["x"], positions["y"], positions["z"], positions["ox"], positions["oy"], positions["oz"])
+        move_object(folder, pose)
+
 
 def _spawn_whole_room(room_name, layout_no, layout_file="layout.yaml"):
     path_to_room = path_to_models + room_name + '/'
@@ -91,6 +172,19 @@ def _spawn_whole_room(room_name, layout_no, layout_file="layout.yaml"):
         orientation = (positions['ox'], positions['oy'], positions['oz']) \
                         if len({'ox','oy','oz'} & set(positions.keys()))==3 else None
         _spawn_single_object(absolute_path, position, orientation, object_name=folder, )
+
+def _debuild_whole_room():
+    room_objects = shelve.open("room_objects")
+    furnitures = room_objects["furniture"]
+
+    blank_pose = (10,10,0,0,0,0)
+
+    for furniture in furnitures:
+        move_object(furniture,  blank_pose)
+    
+    room_objects["furniture"] = []
+    room_objects.close()
+
 
 def _delete_whole_room():
     room_objects = shelve.open("room_objects")
@@ -134,11 +228,14 @@ def spawn(object_dir, position_str, room_name="", layout_no=0):
             name of the room folder. kitchen, bed_room etc. Defaults to "".
     """
     if room_name:
-        _spawn_whole_room(room_name, layout_no)
-    else:
+        _build_whole_room(room_name, layout_no)
+    elif object_dir:
         position = [float(v) for v in position_str.split(',')]
         object_dir = path_to_models + object_dir + '/'
         _spawn_single_object(object_dir, position, None)
+    else:
+        _spawn_all_objects()
+        
 
 def set_parser():
     logger.info("Process is started...")
@@ -160,12 +257,29 @@ def set_parser():
         "--layout",
         help="which layout you want to spawn. Takes integer value."
     )
+    parser.add_argument(
+        "-a",
+        "--all",
+        help="Spawn all objects in the experiment room folders."
+    )
+    parser.add_argument(
+        "-d",
+        "--debuild",
+        help="Move all objects in scene to a blank position, if 1."
+    )
     return parser.parse_args()
+
 
 if __name__ == '__main__':
     args = set_parser()
-    room_name = args.room
-    object_dir = args.object
-    positions = args.positions
-    layout_no = args.layout
-    spawn(object_dir, positions, room_name, layout_no)
+    debuild = args.debuild
+    if debuild == "1":
+        print("Debuilding the room...")
+        _debuild_whole_room()
+    else:
+        print("Spawning...")
+        room_name = args.room
+        object_dir = args.object
+        positions = args.positions
+        layout_no = args.layout
+        spawn(object_dir, positions, room_name, layout_no)
